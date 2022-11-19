18a307bc639971a40bbd71a8a8c44ce0de0bf72e
t0nylombardi <alombardi.331@gmail.com>
2022-11-19 13:15:09 -0500

    add pixel blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,19 @@ export default defineConfig({
   output: 'server',
   adapter: netlify(),
   integrations: [react()],
+  markdown: {
+    shikiConfig: {
+      // Choose from Shiki's built-in themes (or add your own)
+      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+      theme: 'dracula',
+      // Add custom languages
+      // Note: Shiki has countless langs built-in, including .astro!
+      // https://github.com/shikijs/shiki/blob/main/docs/languages.md
+      langs: [],
+      // Enable word wrap to prevent horizontal scrolling
+      wrap: true,
+    },
+  },
 });
 
 devOptions: {
