0efd20cb90ea3d6232bc4e26e5336e1d7dd69446
t0nylombardi <alombardi.331@gmail.com>
2025-03-12 17:00:56 -0400

    feat: enhance contact form with submission handling

--- src/components/ContactForm/NewContactForm.tsx
+++ src/components/ContactForm/NewContactForm.tsx
@@ -2,12 +2,36 @@ import React, {useState} from 'react'
 import CodeHighlight from '../CodeHighlight'
 import './style.css'
 
+interface FormData {
+  name: string
+  email: string
+  message: string
+}
+
 export default function NewContactForm() {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
     message: '',
   })
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    const encode = (data: Record<string, string>) => {
+      return Object.keys(data)
+        .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+        .join('&')
+    }
+
+    fetch('/', {
+      method: 'POST',
+      headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+      body: encode({'form-name': 'contact', ...formData}),
+    })
+      .then(() => (window.location.href = '/form_success'))
+      .catch((error) => alert(error))
+
+    e.preventDefault()
+  }
 
   const truncate = (str, maxLength) => (str.length > maxLength ? str.slice(0, maxLength) + '...' : str)
 
@@ -38,10 +62,16 @@ end
           <form
             id="contact-form"
             className="flex justify-center flex-col w-full "
-            action="/form_success"
-            method="POST"
+            onSubmit={handleSubmit}
             data-netlify="true"
+            data-netlify-recaptcha="true"
+            data-netlify-honeypot="bot-field"
           >
+            <p className="hidden">
+              <label>
+                Don’t fill this out if you’re human: <input name="bot-field" />
+              </label>
+            </p>
             <input
               id="name"
               name="name"
@@ -72,27 +102,12 @@ end
               value={formData.message}
               onChange={handleChange}
             ></textarea>
+            <div data-netlify-recaptcha="true"></div>
             <button
               id="submit-form"
               type="submit"
               className="border border-[#A478E8] hover:text-[#A478E8] font-bold mt-8 p-4"
             >
-              <div id="loader" className="flex items-center ">
-                <svg
-                  className="mr-3 h-5 w-5 animate-spin text-white"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                >
-                  <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
-                  <path
-                    className="opacity-75"
-                    fill="currentColor"
-                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                  ></path>
-                </svg>
-                Loading ...
-              </div>
               <p id="sender">Send Message</p>
             </button>
           </form>
