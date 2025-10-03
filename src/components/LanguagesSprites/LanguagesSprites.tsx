0a877e110c5b1799c33bc4e5b3164872e73e3549
t0nylombardi <alombardi.331@gmail.com>
2025-10-03 17:17:52 -0400

    feat: update LanguagesSprites component with new icons and styles
    - Replaced existing icons with new ones from the Catppuccin theme in LanguagesSprites.tsx
    - Adjusted icon sizes and styles for better consistency
    - Modified showcase max-width in styles.css for improved layout
    - Updated text gradient background in styles.css for better visual appeal
    
    fix: enhance navigation links in Nav.astro
    
    - Added missing blog link in navigation routes
    - Changed resume link to point to the correct section
    
    style: improve AboutSection layout
    
    - Adjusted text size for AboutMe paragraph in AboutSection.tsx
    
    chore: clean up index.astro structure
    
    - Removed unused ProjectSection import
    - Refactored layout for better responsiveness in index.astro
    
    style: update button styles in utilities.css
    
    - Increased button text size for better visibility

--- src/components/LanguagesSprites/LanguagesSprites.tsx
+++ src/components/LanguagesSprites/LanguagesSprites.tsx
@@ -5,27 +5,30 @@ const LanguagesSprites = () => {
   return (
     <div className="flex-1 flex justify-start items-center">
       <div className="flex flex-wrap showcase gap-16 mt-12 justify-center">
-        <Icon icon="logos:ruby" />
-        <Icon icon="logos:rails" />
-        <Icon icon="vscode-icons:file-type-html" />
-        <Icon icon="devicon:css3" />
+        <Icon icon="catppuccin:ruby" width="128" height="128" />
+        <Icon icon="devicon-plain:rails" width="128" height="128" style={{color: '#91d7e3'}} />
+        <Icon icon="catppuccin:typescript-react" width="128" height="128" />
+        <Icon icon="catppuccin:typescript" width="128" height="128" />
 
-        <Icon icon="logos:javascript" />
-        <Icon icon="logos:typescript-icon" />
-        <Icon icon="logos:react" />
-        <Icon icon="devicon:astro" />
+        <Icon icon="catppuccin:javascript" width="128" height="128" />
+        <Icon icon="catppuccin:astro" width="128" height="128" />
+        <Icon icon="catppuccin:html" width="128" height="128" />
+        <Icon icon="catppuccin:css" width="128" height="128" />
 
-        <Icon icon="logos:nodejs-icon" />
-        <Icon icon="logos:postgresql" />
-        <Icon icon="logos:docker-icon" />
-        <Icon icon="logos:go" />
+        <Icon icon="catppuccin:folder-node" width="128" height="128" />
+        <Icon icon="catppuccin:database" width="128" height="128" />
+        <Icon icon="catppuccin:docker" width="128" height="128" />
+        <Icon icon="catppuccin:go" width="128" height="128" />
 
-        <Icon icon="ion:logo-github" className="text-azure" />
-        <div className="hidden sm:block md:hidden lg:block">
-          <Icon icon="emojione:taco" />
-        </div>
-        <Icon icon="vscode-icons:file-type-vscode" />
-        <Icon icon="vscode-icons:file-type-tailwind" />
+        <Icon icon="catppuccin:git" className="text-azure" />
+        <Icon icon="catppuccin:vscode" />
+        <Icon icon="catppuccin:tailwind" />
+
+        <Icon icon="catppuccin:python" width="128" height="128" />
+        <Icon icon="catppuccin:folder-aws" width="128" height="128" />
+        <Icon icon="catppuccin:next" width="128" height="128" />
+        <Icon icon="catppuccin:api-blueprint" width="128" height="128" />
+        <Icon icon="catppuccin:pnpm" width="128" height="128" />
       </div>
     </div>
   )
